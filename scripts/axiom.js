2f60c0ab4fc9f17def9ad242abcf64a1186f7268
Kristy "Orrery" Tracer <orrery@prismaticmedia.com>
2020-05-30 20:50:41 -0700

    Rewrite using buttons and dynamic DOM

--- scripts/axiom.js
+++ scripts/axiom.js
@@ -42,7 +42,7 @@
  *  21 silver
  *  22 gold
  *  23 sun
- *  24
+ *  24 moon
  *  25 mer
  *  26 ve
  *  27 ma
@@ -61,23 +61,157 @@
  * -- Zero
  */
 
-class Player {
-	constructor() {
-		this.red_natural = 0;
+const jacketNumberDisplay = document.querySelector('.jacketNumberDisplay');
+const jacketClickers = document.querySelector('.jacketClickers');
+const jacketPerms = document.querySelector('.jacketPerms');
+const saveLink = document.querySelector('.saveLink');
+const loadLink = document.querySelector('.loadLink');
+const resetLink = document.querySelector('.resetLink');
+const DENI_CONSTANT = Math.pow(Math.E, (Math.PI / Math.SQRT2))
+const PHI = (1 + Math.sqrt(5))/2
+const ORDERS = [
+		"First",
+		"Second",
+		"Third",
+		"Fourth",
+		"Fifth",
+		"Sixth",
+		"Seventh",
+		"Eighth",
+		"Ninth",
+		"Tenth",
+		"Eleventh",
+		"Twelfth",
+		"Thirteenth",
+		"Fourteenth",
+		"Fifteenth",
+		"Sixteenth",
+		"Seventeenth",
+		"Eighteenth",
+		"Nineteenth",
+		"Twentieth",
+		"Twenty-First",
+		"Twenty-Second",
+		"Twenty-Third",
+		"Twenty-Fourth",
+		"Twenty-Fifth",
+		"Twenty-Sixth"
+];
+
+function triangle(num) {
+	var i = 0;
+	for (j = 1; j <= num; j++) {
+		i = i + j;
+	}
+	return i;
+}
+
+
+function render(num) {
+	if (num < 1000) {
+		return num.toString(10);
+	} else {
+		return num.toExponential(3).replace("e+", "&times;10<sup>") + "<sup>";
 	}
 }
 
-function print_obj(p) {
-	document.getElementById("axiom").innerHTML =
-		"Content-Type: text/html"
-		+ "<h1 label=red_natural>"
-		+ p.red_natural
-		+ "</h1>";
+function newPlayer() {
+	var player = {
+		jacket: "red",
+		red: {
+			redNumber: 0,
+			redSuccessors: {
+				redSuccCount: [],
+				redEchoCount: [],
+				redSuccBase: [],
+				redSuccNext: [],
+				redSuccNextFactor: [],
+				redEchoInterval: [],
+				redEchoProbability: [],
+				redSuccVisible: []
+			}
+		}
+	}
+	for (i = 0; i < 24; i++) {
+		player.red.redSuccessors.redSuccCount.push(0);
+		player.red.redSuccessors.redEchoCount.push(0);
+		player.red.redSuccessors.redSuccBase.push(Math.ceil(Math.pow(DENI_CONSTANT, triangle(i+1))));
+		player.red.redSuccessors.redSuccNext.push(Math.ceil(Math.pow(DENI_CONSTANT, triangle(i+1))));
+		player.red.redSuccessors.redSuccNextFactor.push(2 - 1/(1 + i/DENI_CONSTANT));
+		player.red.redSuccessors.redEchoInterval.push(Math.pow(PHI,i));
+		player.red.redSuccessors.redEchoProbability.push(1 / Math.pow(i+1));
+		player.red.redSuccessors.redSuccVisible.push(false);
+	}
+	return player;
 }
 
-function main() {
-	let player_obj = new Player() // Let there be.
-	print_obj(player_obj)
+function loadPlayer() {
+	var player;
+	if (localStorage.getItem("axiomPlayer")) {
+		player = JSON.parse(localStorage.getItem("axiomPlayer"));
+		console.log("Loading: %o", player);
+	} else {
+		player = newPlayer();
+		console.log("Creating new player.");
+	}
+	return player;
+}
+
+function savePlayer(player) {
+	localStorage.setItem("axiomPlayer", JSON.stringify(player));
+	console.log("Saving.");
+}
+
+function resetPlayer() {
+	localStorage.removeItem("axiomPlayer");
+	console.log("Reseting.");
+}
+
+function redClickSuccessor(r) {
+	console.log("before r.redNumber: " + r.redNumber);
+	r.redNumber = r.redNumber + 1;
+	console.log("after r.redNumber: " + r.redNumber);
+	redDisplayNumber(r);
+}
+
+function redDisplayNumber(r) {
+	var num = document.createElement("h1");
+	num.setAttribute("id", "redNumber");
+	num.appendChild(document.createTextNode(render(r.redNumber)));
+	jacketNumberDisplay.replaceChild(num, jacketNumberDisplay.childNodes[0]);
+}
+	
+
+function createBoard(player) {
+	redDisplayNumber(player.red);
+	var btn = document.createElement("button");
+	btn.appendChild(document.createTextNode("Zeroth-Order Successor"));
+	btn.setAttribute("id", "redButton");
+	btn.classList.add("redZerothSuccessor");
+	btn.addEventListener('click', function() {
+		redClickSuccessor(player.red);
+	});
+	jacketClickers.appendChild(btn);
+}
+
+function main() {  // Let there be.
+	var player_obj = loadPlayer();
+	loadLink.addEventListener('click', function() {
+		player_obj = loadPlayer();
+	});
+	saveLink.addEventListener('click', function() {
+		savePlayer(player_obj);
+	});
+	resetLink.addEventListener('click', function() {
+		resetPlayer();
+		player_obj = newPlayer();
+		window.location.reload(false);
+	});
+	createBoard(player_obj);
 }
 
 main() // And there was.
+
+// ---
+
+
